8b39815cfd0715128e66b10841f66668b9fe5b04
Mark Christian Cruz <mcrcruz15@gmail.com>
2022-11-21 19:29:11 +0800

    edited landing page

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+ $('.popup-youtube, .popup-text').magnificPopup({
+                     disableOn: 320,
+                     type: 'iframe',
+                     mainClass: 'mfp-fade',
+                     removalDelay: 160,
+                     preloader: false,
+                     fixedContentPos: true
+ });
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+$('.image-zoom').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+});
