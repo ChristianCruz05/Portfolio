053e7e63cdcf8a2aef21101285ab07c7040f92a7
Mark Christian Cruz <mcrcruz15@gmail.com>
2022-12-02 16:13:27 +0800

    added sweet alert and linked socmeds

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+
  $('.popup-youtube, .popup-text').magnificPopup({
                      disableOn: 320,
                      type: 'iframe',
@@ -26,3 +27,23 @@ $(function(){
     $("#neon-button").toggle();
     $("#neon-button").fadeIn(1200);
 })
+
+$( document ).ready(function() {
+	console.log("FIRE");
+    Swal.fire({
+  	text: 'Click the neon name to watch my Video Resume',
+ 	icon: 'info',
+	toast: 'true',
+	position: 'top',
+	color: '#CCC5B9',
+	iconColor: '#E18937',
+	background: '#403D39',
+	timer: "4000",
+	timerProgressBar: 'true',
+	width: '30rem',
+	confirmButtonColor: '#335C67'
+})
+});
+
+
+
